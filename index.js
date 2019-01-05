d8d7665b308040aa22c258dd06fd1036eaf60472
Christina Dong <christinadongla@yahoo.com>
2019-01-05 21:50:51 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -14,10 +14,9 @@ function logWhisper(string) {
   console.log(string.toLowerCase())
 }
 
-
 function sayHiToGrandma(string) {
   if (string==="lowercase") {
-    return "I can\'t hear you!";
+    return "I can't hear you!";
   } else if (string==="uppercase") {
     return "YES INDEED!";
   } else if (string==="I love you, Grandma.") {
